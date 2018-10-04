616ce9b6b264b527a2343f756852b0ce60eb5d55
Wenish <jonas.voland@bluewin.ch>
2018-10-04 21:56:13 +0200

    added vscode plugin

---
+++ Assets/Scripts/Walk.cs
@@ -0,0 +1,11 @@
+using UnityEngine;
+
+namespace UnityEcsTest.Assets.Scripts
+{
+    public class Walk : MonoBehaviour
+    {
+        void Start(){
+            Debug.Log("Hello from walk");
+        }
+    }
+}
